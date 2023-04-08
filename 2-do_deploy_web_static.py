74d8aef802bb69ef43e0ebf8781ceaa7992d8ac2
alex <alexmuthini3@gmail.com>
2023-04-08 11:55:00 +0300

    added 2-do_deploy_web_static

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Based on '1-pack_web_static.py'. It distributes an archive to my web
+    servers using the function 'do_deploy'.
+"""
+from fabric.api import env, put, run, sudo
+from os.path import exists
+env.hosts = ['18.209.223.27', '100.25.118.39']
+env.user = 'ubuntu'
+
+
+def do_deploy(file_path):
+    """ It distributes an archive to my web servers."""
+    if not exists(file_path):
+        print('file doesnt exist')
+        return False
+    try:
+        filename = file_path.split('/')[-1]
+        file = filename.split('.')[0]
+        #run('mkdir -p /tmp/{}'.format(file_path))
+        put(file_path, '/tmp/')
+        sudo('mkdir -p /data/web_static/releases/{}'.format(file))
+        sudo('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+                .format(filename, file))
+        run('rm -f /tmp/{}'.format(filename))
+        run('rm -f /data/web_static/current')
+        run("ln -sf /data/web_static/releases/{}/web_static/ /data/web_static/current"
+                .format(file))
+
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+if __name__ == "__main__":
+    #env.hosts = ['web-01.alexmuthini.tech', 'web-02.alexmuthini.tech']
+    #env.user = 'ubuntu'
+    ret = do_deploy('versions/web_static_20230407191528.tgz')
+    print(ret)
