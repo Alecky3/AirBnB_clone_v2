c75ba58a6ea24478db4bf055687127224188c620
alex <alexmuthini3@gmail.com>
2023-04-22 20:26:07 +0300

    added 4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Starts a Flask web application."""
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ home view that return 'Hello HBNB'."""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ hbnb view that returns 'HBNB'."""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def croute(text):
+    """croute view return 'c <text>'."""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonroute(text="is cool"):
+    """Python view, returns 'Python <text>'."""
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>')
+def numberroute(n):
+    """ number route, returns 'n is a number if n is a number'."""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
