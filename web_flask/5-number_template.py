a034c775dc058031f6d0236390381365e9820419
alex <alexmuthini3@gmail.com>
2023-04-22 20:41:11 +0300

    added 5-number_template.py

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Starts a Flask web application."""
+from flask import Flask, render_template
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ home view that return 'Hello HBNB'."""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ hbnb view that returns 'HBNB'."""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def croute(text):
+    """croute view return 'c <text>'."""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonroute(text="is cool"):
+    """Python view, returns 'Python <text>'."""
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>')
+def numberroute(n):
+    """ number route, returns 'n is a number if n is a number'."""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numbertemplate(n):
+    """ numberteplate view return the teplate 5-number.html."""
+    return render_template('5-template.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
